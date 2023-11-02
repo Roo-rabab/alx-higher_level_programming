4ae375b12b5f51f28c21cf7aa0bbb0eea9d34c63
Roo-rabab <rababmchich312@gmail.com>
2023-11-02 09:20:09 -0700

    network_0

--- 0x10-python-network_0/5-post_params.sh
+++ 0x10-python-network_0/5-post_params.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-# Send a GET request to a given URL with a header variable.
-curl -sH "X-HolbertonSchool-User-Id: 98" "${1}"
+# Bash script that takes in a URL, sends a POST request to the passed URL, and displays the body of the response
+curl -s -X POST -d "email=test@gmail.com&subject=I will always be here for PLD" "$1"
